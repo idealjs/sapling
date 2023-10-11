0cc54f4496047276fb97e7a54c46ad7efb220b6b
cqh963852 <cqh963852@hotmail.com>
2023-10-11 23:30:22 +0800

    chore: add reactive test

---
+++ packages/core/src/reactive.spec.ts
@@ -0,0 +1,90 @@
+import fs from "fs";
+import { writeHeapSnapshot } from "v8";
+import { describe, expect, it, vi } from "vitest";
+
+import { createState, derive, effect } from "./reactive";
+import { getNodes } from "./utils/v8";
+
+describe("unit test", () => {
+  it("derive state val", () => {
+    const state = createState(1);
+    const derived = derive(() => {
+      return state.val + 1;
+    });
+
+    expect(derived.val).toBe(2);
+    expect(derived.oldVal).toBe(undefined);
+
+    state.val++;
+
+    expect(derived.val).toBe(3);
+    expect(derived.oldVal).toBe(2);
+  });
+  it("derive state oldVal", () => {
+    const state = createState(1);
+    const derived = derive(() => {
+      return (state.oldVal ?? 0) + 1;
+    });
+
+    expect(derived.val).toBe(1);
+    expect(derived.oldVal).toBe(undefined);
+
+    state.val++;
+
+    expect(derived.val).toBe(2);
+    expect(derived.oldVal).toBe(1);
+  });
+  it("derive stateView", () => {
+    const state = createState(1);
+    const derived = derive(() => {
+      return state.val + 1;
+    });
+
+    const test = derive(() => {
+      return derived.val * derived.val;
+    });
+
+    expect(test.val).toBe(4);
+    expect(test.oldVal).toBe(undefined);
+
+    state.val++;
+
+    expect(test.val).toBe(9);
+    expect(test.oldVal).toBe(4);
+  });
+  it.only("state in derive", () => {
+    vi.useFakeTimers();
+    const stubFn = vi.fn();
+    const interval = createState(1000);
+    const dispose = effect(() => {
+      const handler = setInterval(stubFn, interval.val);
+      return () => {
+        clearInterval(handler);
+      };
+    });
+
+    const snapshot1 = writeHeapSnapshot();
+    vi.advanceTimersToNextTimer();
+    expect(stubFn).toBeCalledTimes(1);
+
+    // Note that a timer clear is done here
+    dispose.val();
+
+    interval.val = 2000;
+    const snapshot2 = writeHeapSnapshot();
+    vi.advanceTimersByTime(2000);
+    expect(stubFn).toBeCalledTimes(2);
+    expect(
+      getNodes(JSON.parse(fs.readFileSync(snapshot1).toString())).filter(
+        (node) => node.name.includes("class State"),
+      ).length,
+    ).toMatchInlineSnapshot("2");
+    expect(
+      getNodes(JSON.parse(fs.readFileSync(snapshot2).toString())).filter(
+        (node) => node.name.includes("class State"),
+      ).length,
+    ).toMatchInlineSnapshot("2");
+    fs.unlinkSync(snapshot1);
+    fs.unlinkSync(snapshot2);
+  });
+});
---
+++ packages/core/src/utils/v8.ts
@@ -0,0 +1,123 @@
+import chunk from "lodash.chunk";
+export type HeapSnapshot = {
+  snapshot: {
+    meta: {
+      node_fields: [
+        "type",
+        "name",
+        "id",
+        "self_size",
+        "edge_count",
+        "trace_node_id",
+        "detachedness",
+      ];
+      node_types: [
+        [
+          "hidden",
+          "array",
+          "string",
+          "object",
+          "code",
+          "closure",
+          "regexp",
+          "number",
+          "native",
+          "synthetic",
+          "concatenated string",
+          "sliced string",
+          "symbol",
+          "bigint",
+        ],
+        "string",
+        "number",
+        "number",
+        "number",
+        "number",
+        "number",
+      ];
+      edge_fields: ["type", "name_or_index", "to_node"];
+      edge_types: [
+        [
+          "context",
+          "element",
+          "property",
+          "internal",
+          "hidden",
+          "shortcut",
+          "weak",
+        ],
+        "string_or_number",
+        "node",
+      ];
+      trace_function_info_fields: [
+        "function_id",
+        "name",
+        "script_name",
+        "script_id",
+        "line",
+        "column",
+      ];
+      trace_node_fields: [
+        "id",
+        "function_info_index",
+        "count",
+        "size",
+        "children",
+      ];
+      sample_fields: ["timestamp_us", "last_assigned_id"];
+      location_fields: ["object_index", "script_id", "line", "column"];
+    };
+    node_count: number;
+    edge_count: number;
+    trace_function_count: number;
+  };
+  nodes: number[];
+  edges: number[];
+  trace_function_infos: [];
+  trace_tree: [];
+  samples: [];
+  locations: number[];
+  strings: string[];
+};
+
+export type HeapNode = {
+  type: string;
+  name: string;
+  id: number;
+  selfSize: number;
+  edgeCount: number;
+  traceNodeId: number;
+  detachedness: number;
+};
+
+export const getNodes = (heapSnapshot: HeapSnapshot) => {
+  const nodes = (
+    chunk(heapSnapshot.nodes, 7) as [
+      number,
+      number,
+      number,
+      number,
+      number,
+      number,
+      number,
+    ][]
+  ).map((node) => {
+    const type = heapSnapshot.snapshot.meta.node_types[0][node[0]];
+    const name = heapSnapshot.strings[node[1]];
+    const id = node[2];
+    const size = node[3];
+    const edgeCount = node[4];
+    const traceNodeId = node[5];
+    const detachedness = node[6];
+    return {
+      type,
+      name,
+      id,
+      size,
+      edgeCount,
+      traceNodeId,
+      detachedness,
+    };
+  });
+  return nodes;
+};
