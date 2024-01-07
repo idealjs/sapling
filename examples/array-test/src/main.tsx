717db1956105914b2969f7a03d6054077657c2ce
cqh963852 <cqh963852@hotmail.com>
2024-01-08 02:46:18 +0800

    chore: add array-test example

---
+++ examples/array-test/src/main.tsx
@@ -0,0 +1,20 @@
+import { upsert } from "@idealjs/sapling";
+
+const Test = () => {
+  return [<div>A</div>, <div>B</div>];
+};
+
+const Component = () => {
+  return (
+    <div>
+      <div>{() => <Test />}</div>
+      <div>{() => [<Test />, <Test />]}</div>
+      <Test />
+      [ <Test />,<Test />,<Test />]
+    </div>
+  );
+};
+
+const root = document.getElementById("app")!;
+
+upsert(root, <Component />);
---
+++ examples/array-test/vite.config.ts
@@ -0,0 +1,12 @@
+import path from "path";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [],
+  resolve: {
+    alias: {
+      "@idealjs/sapling": path.resolve(__dirname, "../../packages/core/src"),
+    },
+  },
+});
