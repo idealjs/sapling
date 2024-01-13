6367c3c8a8aa4ffa3714029bcbbc2ec18367edfb
cqh963852 <cqh963852@hotmail.com>
2024-01-14 00:56:01 +0800

    chore: add test case

---
+++ examples/nested-test/src/main.tsx
@@ -0,0 +1,39 @@
+import { createProxy, createRef, upsert, useEffect } from "@idealjs/sapling";
+import { JSX } from "@idealjs/sapling/jsx-runtime";
+
+const counter = createProxy({ val: 0 });
+
+const Test = () => {
+  return (
+    <div>
+      {() => counter.val}
+      <button
+        onClick={() => {
+          counter.val++;
+        }}
+      >
+        plus
+      </button>
+    </div>
+  );
+};
+
+const Component = () => {
+  const Comp = createRef<() => JSX.Element>();
+  useEffect(() => {
+    console.log("test test panel useEffect");
+    Comp.current = Test;
+  });
+  return (
+    <div>
+      {() => {
+        console.log("test test Comp.current");
+        return Comp.current ? <Comp.current /> : null;
+      }}
+    </div>
+  );
+};
+
+const root = document.getElementById("app")!;
+
+upsert(root, <Component />);
