55b81062a660e0de62fabb87e983d1acc78061bc
cqh963852 <cqh963852@hotmail.com>
2024-01-29 18:35:22 +0800

    temp

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "jsdom",
+    setupFiles: path.resolve(__dirname, "./vitest.setup.ts"),
   },
   resolve: {
     alias: {
