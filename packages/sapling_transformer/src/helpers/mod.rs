7db528209ee6627ac3f498c85af441e58dd5e9a2
cqh963852 <cqh963852@hotmail.com>
2025-08-22 14:09:31 +0800

    chore: clean code

--- packages/sapling_transformer/src/helpers/mod.rs
+++ packages/sapling_transformer/src/helpers/mod.rs
@@ -1,46 +1,38 @@
-use biome_js_factory::make::{
-    js_expression_statement, js_function_body, js_identifier_binding, js_identifier_expression,
-    js_initializer_clause, js_parameters, js_parenthesized_expression, js_reference_identifier,
-    js_variable_declaration,
-};
-use biome_js_syntax::{
-    AnyJsAssignment, AnyJsAssignmentPattern, AnyJsBinding, AnyJsBindingPattern, AnyJsCallArgument,
-};
-use biome_js_syntax::{JsAssignmentExpression, JsCallArguments};
-use biome_rowan::{AstNode, BatchMutation, BatchMutationExt};
+use biome_js_factory::make::{js_function_body, js_parameters, js_parenthesized_expression};
+use biome_js_syntax::AnyJsCallArgument;
+use biome_js_syntax::JsCallArguments;
 use std::vec;
 
 use biome_js_factory::make::{
-    js_arrow_function_expression, js_assignment_expression, js_call_argument_list,
-    js_call_arguments, js_call_expression, js_name, js_return_statement, js_statement_list,
-    js_static_member_assignment, js_variable_declarator, js_variable_declarator_list,
-    js_variable_statement, token,
+    js_arrow_function_expression, js_call_argument_list, js_call_arguments, js_call_expression,
+    js_return_statement, token,
 };
 use biome_js_syntax::{
-    AnyJsExpression, AnyJsStatement, AnyJsxChild, JsArrowFunctionExpression, JsCallExpression,
-    JsDirectiveList, JsFunctionBody, JsLanguage, JsParameterList, JsParameters, JsReturnStatement,
-    JsStatementList, JsSyntaxToken, JsxElement, JsxTagExpression, T,
+    AnyJsExpression, JsArrowFunctionExpression, JsCallExpression, JsDirectiveList, JsFunctionBody,
+    JsParameterList, JsParameters, JsReturnStatement, JsStatementList, JsSyntaxToken, T,
 };
 
-use biome_rowan::SyntaxToken;
-
+pub mod jsx_element_name_to_string;
 pub mod make_array;
+pub mod make_arrow_function_from_statements;
 pub mod make_create_element;
 pub mod make_create_jsx_tag_element;
 pub mod make_create_text_node;
+pub mod make_effect;
 pub mod make_insert;
 pub mod make_insert_node;
 pub mod make_set_prop;
-pub mod jsx_element_name_to_string;
 
+pub use jsx_element_name_to_string::*;
 pub use make_array::*;
+pub use make_arrow_function_from_statements::*;
 pub use make_create_element::*;
 pub use make_create_jsx_tag_element::*;
 pub use make_create_text_node::*;
+pub use make_effect::*;
 pub use make_insert::*;
 pub use make_insert_node::*;
 pub use make_set_prop::*;
-pub use jsx_element_name_to_string::*;
 
 pub fn make_js_call_expression(
     arrow_function_expression: JsArrowFunctionExpression,
@@ -91,10 +83,6 @@ pub fn make_js_parameters(
     js_parameters(token(T!['(']), js_parameter_list, token(T![')']))
 }
 
-pub fn make_members() -> JsStatementList {
-    js_statement_list(vec![])
-}
-
 pub fn make_js_return_statement(any_js_expression: AnyJsExpression) -> JsReturnStatement {
     js_return_statement(token(T![return]))
         .with_argument(any_js_expression)
@@ -107,159 +95,4 @@ pub fn make_js_function_body(
     statements: JsStatementList,
 ) -> JsFunctionBody {
     js_function_body(token(T!['{']), directives, statements, token(T!['}']))
-}
-
-pub fn collect_jsx_element_tags(node: &JsxElement) {
-    let opening_element = node
-        .opening_element()
-        .expect("JsxElement should have an opening element");
-    let name = opening_element
-        .name()
-        .expect("Opening element should have a name");
-    let _attr = opening_element.attributes();
-    let jsx_name = name.as_jsx_name().expect("Name should be a JsxName");
-    let value_token = jsx_name
-        .value_token()
-        .expect("JsxName should have a value token");
-    let _name = value_token.text();
-    let child_list = node.children();
-    child_list.into_iter().for_each(|child| match child {
-        AnyJsxChild::JsxElement(jsx_element) => {
-            collect_jsx_element_tags(&jsx_element);
-        }
-        _ => {}
-    });
-}
-
-pub fn collect_jsx_tag_expression(node: &JsxTagExpression) {
-    let tag = node.tag().expect("JsxTagExpression should have a tag");
-    let jsx_element = tag.as_jsx_element().expect("Tag should be a JsxElement");
-    collect_jsx_element_tags(jsx_element);
-}
-
-pub struct StatementItemConfig {
-    pub el_var: String,                        // "_el$"
-    pub tmpl_fn: String,                       // "_tmpl$"
-    pub event_bindings: Vec<(String, String)>, // [("$$click", "increment")]
-    pub inserts: Vec<(String, String)>,        // [("_el$", "count")]
-    pub return_var: String,                    // "_el$"
-}
-
-pub fn make_statement_items(config: &StatementItemConfig) -> Vec<AnyJsStatement> {
-    let mut stmts = Vec::new();
-
-    // 1. var _el$ = _tmpl$();
-    let el_var_token = SyntaxToken::new_detached(T![ident], &config.el_var, Vec::new(), Vec::new());
-    let tmpl_fn_token =
-        SyntaxToken::new_detached(T![ident], &config.tmpl_fn, Vec::new(), Vec::new());
-    let el_var = js_reference_identifier(el_var_token.clone());
-    let tmpl_fn = js_reference_identifier(tmpl_fn_token.clone());
-    let declarator = js_variable_declarator(AnyJsBindingPattern::AnyJsBinding(
-        AnyJsBinding::JsIdentifierBinding(js_identifier_binding(el_var_token.clone())),
-    ))
-    .with_initializer(
-        js_initializer_clause(
-            token(T![=]),
-            js_call_expression(
-                js_identifier_expression(tmpl_fn.clone()).into(),
-                make_js_call_arguments(vec![], vec![]),
-            )
-            .build()
-            .into(),
-        )
-        .into(),
-    )
-    .build();
-    let var_decl = js_variable_statement(
-        js_variable_declaration(
-            token(T![var]),
-            js_variable_declarator_list(vec![declarator], vec![]),
-        )
-        .build(),
-    )
-    .build();
-    stmts.push(var_decl.into());
-
-    // 2. _el$.$$click = increment;
-    for (event, handler) in &config.event_bindings {
-        let event_token = SyntaxToken::new_detached(T![ident], event, Vec::new(), Vec::new());
-        let handler_token = SyntaxToken::new_detached(T![ident], handler, Vec::new(), Vec::new());
-        let member = js_static_member_assignment(
-            js_identifier_expression(el_var.clone()).into(),
-            token(T![.]),
-            js_name(event_token).into(),
-        );
-        let assign = js_expression_statement(
-            make_js_assignment_expression(
-                member.into(),
-                js_identifier_expression(js_reference_identifier(handler_token)).into(),
-            )
-            .into(),
-        )
-        .build();
-        stmts.push(assign.into());
-    }
-
-    // 3. _$insert(_el$, count);
-    for (el, value) in &config.inserts {
-        let el_token = SyntaxToken::new_detached(T![ident], el, Vec::new(), Vec::new());
-        let value_token = SyntaxToken::new_detached(T![ident], value, Vec::new(), Vec::new());
-        let insert_token = SyntaxToken::new_detached(T![ident], "_$insert", Vec::new(), Vec::new());
-        let call = js_expression_statement(AnyJsExpression::JsCallExpression(
-            js_call_expression(
-                js_identifier_expression(js_reference_identifier(insert_token)).into(),
-                make_js_call_arguments(
-                    vec![
-                        AnyJsCallArgument::AnyJsExpression(
-                            js_identifier_expression(js_reference_identifier(el_token.clone()))
-                                .into(),
-                        ),
-                        AnyJsCallArgument::AnyJsExpression(
-                            js_identifier_expression(js_reference_identifier(value_token.clone()))
-                                .into(),
-                        ),
-                    ],
-                    vec![],
-                ),
-            )
-            .build(),
-        ))
-        .build();
-        stmts.push(call.into());
-    }
-
-    // 4. return _el$;
-    let ret = make_js_return_statement(js_identifier_expression(el_var).into()).into();
-    stmts.push(ret);
-
-    stmts
-}
-
-pub fn make_js_assignment_expression(
-    left: AnyJsAssignment,
-    right: AnyJsExpression,
-) -> JsAssignmentExpression {
-    js_assignment_expression(
-        AnyJsAssignmentPattern::AnyJsAssignment(left),
-        token(T![=]),
-        right,
-    )
-}
-
-pub fn replace<P, T>(
-    target: &P,
-    get_parent: impl Fn(&P) -> T,
-    get_new_parent: impl Fn(&T) -> T,
-) -> BatchMutation<JsLanguage>
-where
-    P: AstNode<Language = JsLanguage>,
-    T: AstNode<Language = JsLanguage>,
-{
-    let mut mutation = target.clone().begin();
-    let parent = get_parent(&target);
-    let new_node = get_new_parent(&parent);
-
-    mutation.replace_node(parent, new_node);
-
-    mutation
 }
